d7bd3c3c0ab631627739540de37a09a5697408f2
Eyuel <eyuelmulatu@gmail.com>
2020-08-26 09:07:44 +0300

    initial commit - project setup

---
+++ src/main/java/com/spring/boot/minishop/MiniShopRestfullWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.minishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniShopRestfullWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniShopRestfullWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/minishop/MiniShopRestfullWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.minishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniShopRestfullWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
